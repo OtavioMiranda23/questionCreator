3ece69af410da831e3872be6df8cf9ae12cf2ef8
OtavioMiranda23 <otaviomiranda@usp.br>
2024-10-12 01:47:16 -0300

    Refatora para classes

--- src/question.ts
+++ src/question.ts
@@ -1,39 +1,69 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
-export function createQuestion(law: string): string {
-    const regexVerbs =  /\b\w+(ar|er|ir)\b/g;
-    const match = law.match(regexVerbs);
-    if (!match) throw new Error("Verbs not match");
-    const question = law.replace(match[0], "_____");
-    return question;
-}
-export function createResponse(law: string): string {
-    const regexVerbs =  /\b\w+(ar|er|ir)\b/g;
-    const match = law.match(regexVerbs);
-    if (!match) throw new Error("Verbs not match");
-    const response = match[0].trim();
-    return response;
-}
-
-export function createTemplate(question: string, response: string) {
-    return `Complete a lacuna com uma das opções:
-    ${question}
-    Resposta: ${response}`
-}
-
-export async function createAlternatives(word: string) {
-    const res = await axios.get<string>(`https://www.sinonimos.com.br/${word}`);
-    if(!res.data) throw new Error("Word not found");
-    const alternatives: string[] = [];
-    const $ = cheerio.load(res.data);
-    $('.syn-list a').each((i, item) => { 
-        if (item) alternatives.push($(item).text().trim())
-    })
-    const sufix = word.slice(-2);
-    const regexSufix = new RegExp(`\\b\\w+${sufix}\\b`, 'g');
-    const alternativesSufix = alternatives.filter(alternative => regexSufix.test(alternative));
-    const threeAlternatives = [word, alternativesSufix[0], alternativesSufix[1]];
-    return threeAlternatives;
+export class CreateAlternatives {
+    private threeAlternatives: string[] = [];
+    constructor (readonly word: string) {}
     
+    public async execute () {
+        //axios
+        const res = await axios.get<string>(`https://www.sinonimos.com.br/${this.word}`);       
+        if(!res.data) throw new Error("Word not found");
+        const alternatives: string[] = [];        
+        //selection
+        const $ = cheerio.load(res.data);
+        $('.syn-list a').each((i, item) => { 
+            if (item) alternatives.push($(item).text().trim())
+        })
+        //proccess
+        const sufix = this.word.slice(-2);
+        const regexSufix = new RegExp(`\\b\\w+${sufix}\\b`, 'g');
+        const alternativesSufix = alternatives.filter(alternative => regexSufix.test(alternative));        
+        //mount alternatives
+        this.threeAlternatives.push(this.word);
+        this.threeAlternatives.push(alternativesSufix[0]);
+        this.threeAlternatives.push(alternativesSufix[1]);
+    }
+
+    public getThreeAlternatives() {
+        return this.threeAlternatives;
+    }
+}
+
+export class CreateCompleteQuestion {
+    constructor (readonly law: string) {
+    }
+    public execute (): string {
+        const regexVerbs =  /\b\w+(ar|er|ir)\b/g;
+        const match = this.law.match(regexVerbs);
+        if (!match) throw new Error("Verbs not match");
+        const question = this.law.replace(match[0], "_____");
+        return question;
+    }
+}
+
+export class CreateResponse {
+    constructor (readonly law: string) {
+    }
+    public execute (): string {
+        const regexVerbs =  /\b\w+(ar|er|ir)\b/g;
+        const match = this.law.match(regexVerbs);
+        if (!match) throw new Error("Verbs not match");
+        const response = match[0].trim();
+        return response;
+    }
+}
+
+export class TemplateQuestion {
+    private question: string;
+    private response: string;
+
+    constructor (question: string, response: string ) {
+        this.question = question;
+        this.response = response;
+    }
+
+    public createQuestionWithResponse() {
+        return `Complete a lacuna com uma das opções:\n${this.question}\nResposta: ${this.response}`
+    }
 }
--- test/question.test.ts
+++ test/question.test.ts
@@ -1,29 +1,32 @@
-import { createAlternatives, createQuestion, createResponse, createTemplate } from "../src/question";
+import { CreateAlternatives, CreateCompleteQuestion, CreateResponse, TemplateQuestion } from "../src/question";
 
 test("Deve criar uma questão", () => {
     const law = "Omitir, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:";
     const finalQuestion = `_____, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:`
-    expect(createQuestion(law)).toBe(finalQuestion);
-})
-test("Deve criar uma questão completa", () => {
-    const law = "Omitir, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:";
-    const finalQuestion = `_____, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:`
-    const response = createResponse(law);
-    const template = `Complete a lacuna com uma das opções:
-    ${finalQuestion}
-    Resposta: ${response}`;
-    const question = createQuestion(law);
-    expect(createTemplate(question, response)).toBe(template);
-})
-test("Deve dar erro ao criar a questão", () => {
-    const noVerbsLaw = "aaaa";
-    expect(() => createQuestion(noVerbsLaw)).toThrow("Verbs not match");
-    const emptyString = "";
-    expect(() => createQuestion(emptyString)).toThrow("Verbs not match");
+    const question = new CreateCompleteQuestion(law).execute();
+    expect(question).toBe(finalQuestion);
 })
 
-test("Deve criar questão com alternativas", async () => {
+test("Deve dar erro ao criar a questão", () => {
+    const noVerbsLaw = "aaaa";
+    expect(() => new CreateCompleteQuestion(noVerbsLaw).execute()).toThrow("Verbs not match");
+    const emptyString = "";
+    expect(() => new CreateCompleteQuestion(emptyString).execute()).toThrow("Verbs not match");
+})
+
+test("Deve criar uma questão completa", () => {
     const law = "Omitir, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:";
-    const alternatives = await createAlternatives("omitir");
-    expect(alternatives).toHaveLength(3);
+    const question = new CreateCompleteQuestion(law).execute();
+    const response = new CreateResponse(law).execute();
+    const questionComplete = new TemplateQuestion(question, response).createQuestionWithResponse();
+    const template = `Complete a lacuna com uma das opções:\n${question}\nResposta: ${response}`;  
+    expect(questionComplete).toBe(template);
+})
+
+
+test.only("Deve criar questão com alternativas", async () => {
+    const law = "Omitir, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:";
+    const alternatives = new CreateAlternatives("omitir");
+    await alternatives.execute();
+    expect(alternatives.getThreeAlternatives()).toHaveLength(3);
 })
