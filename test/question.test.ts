6887c8061c2f4f6fd8550a9a8769d83bf68aabd0
OtavioMiranda23 <otaviomiranda@usp.br>
2024-10-15 02:28:13 -0300

    implementa use case criar questao

--- test/question.test.ts
+++ test/question.test.ts
@@ -1,4 +1,5 @@
 import {  DifficultyQuestion, LawQuestion } from "../src/question";
+import { CreateQuestion } from "../src/useCase/CreateQuestion";
 
 test("Deve criar uma questão de direito", () => {
     const law = "Art. 299 - Omitir, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:";
@@ -30,10 +31,15 @@ test("Deve criar uma questão com template", () => {
     expect(question).toBe(template);
 })
 
-
-test.only("Deve criar questão com alternativas", async () => {
+test("Deve criar questão com alternativas", async () => {
     const law = "Art. 299 - Omitir, em documento público ou particular, declaração que dele devia constar, ou nele inserir ou fazer inserir declaração falsa ou diversa da que devia ser escrita, com o fim de prejudicar direito, criar obrigação ou alterar a verdade sobre fato juridicamente relevante:";
     const question = new LawQuestion(law, DifficultyQuestion.EASY);
     await question.createAlternatives();
-    expect(question.getAlternatives()).toBeGreaterThan(0);
+    expect(question.getAlternatives().length).toBeGreaterThan(0);
+})
+
+test("Deve criar uma questão com alternativas e simular uma resposta com feedback", async () => {
+    const article = "299";
+    const questions = await new CreateQuestion(article).execute();
+    expect(questions).toBeDefined();
 })
